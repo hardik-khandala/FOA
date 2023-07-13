1d046d05ef310e343271f9e3987e470f20c1e738
Hardik Khandala <90664350+hardik-khandala@users.noreply.github.com>
2023-07-13 12:44:02 +0530

    Create 2-b.cpp

---
+++ 2-b.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int fact(int n) 
+{
+    if (n == 0) {
+        return 1;
+    } else {
+        return n * fact(n - 1);
+    }
+}
+
+int main() {
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+
+    int result = fact(n);
+    cout<<"Factorial is "<<result<<endl;
+
+    return 0;
+}
